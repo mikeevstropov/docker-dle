0637de5338c57f82b813a23442c804a4ae41b9a1
mikeevstropov <mike.evstropov@gmail.com>
2018-05-28 20:44:04 +0300

    Versions added.

---
+++ 11.2-php5.6-apache/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# directory is empty
+if [ -z "$(ls -A /var/www/html)" ]; then
+
+    # extracting
+    unzip dle.zip upload/*
+    mv upload/* /var/www/html
+
+    # clearing
+    rm dle.zip
+    rm -rf upload
+
+    # define owner
+    chown -R www-data:www-data /var/www/html
+fi
+
+exec "$@"
