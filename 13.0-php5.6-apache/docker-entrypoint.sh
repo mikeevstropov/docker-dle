289746d305e5885bbe16954d3a9922309598fd6e
Mike Evstropov <mike.evstropov@gmail.com>
2020-04-29 20:56:14 +0400

    CDN host fix. DLE 13.3 and 14.0 has added.

--- 13.0-php5.6-apache/docker-entrypoint.sh
+++ 13.0-php5.6-apache/docker-entrypoint.sh
@@ -5,16 +5,16 @@ set -euo pipefail
 # directory is empty
 if [ -z "$(ls -A /var/www/html)" ]; then
 
-    # extracting
-    unzip dle.zip upload/*
-    mv upload/* /var/www/html
+  # extracting
+  unzip dle.zip upload/*
+  mv upload/* /var/www/html
 
-    # clearing
-    rm dle.zip
-    rm -rf upload
+  # clearing
+  rm dle.zip
+  rm -rf upload
 
-    # define owner
-    chown -R www-data:www-data /var/www/html
+  # define owner
+  chown -R www-data:www-data /var/www/html
 fi
 
 exec "$@"
